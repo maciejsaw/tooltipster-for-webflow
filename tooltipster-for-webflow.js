0bfe8646075007bd3aa39a333427047ca3ce34f1
Maciej Sawicki <saw.maciej@gmail.com>
2018-12-04 10:56:07 +0100

    Updated for more triggers

--- tooltipster-for-webflow.js
+++ tooltipster-for-webflow.js
@@ -1,71 +1,130 @@
-function tooltipster_init() {
-    $('[tooltipster="top"]').tooltipster({
+var tooltipsterTrigger = 'custom';
+
+var tooltipsterTriggerOpen = {
+    mouseenter: true,
+    touchstart: true,
+    tap: true,
+    click: true
+};
+
+var tooltipsterTriggerClose = {
+    mouseleave: true,
+    scroll: true,
+    tap: true
+};
+
+function initTooltipster(containerToInit) {
+    containerToInit = containerToInit || 'body';
+    console.log(containerToInit);
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="top"]:not(.tooltipstered)').tooltipster({
+        position: 'top',
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
         hideOnClick: false,
         animation: 'fade',
         delay: 20,
-        speed: 150,
-        maxWidth: 300,
-        theme: 'tooltipster-borderless'
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
     });
-    $('[tooltipster="bottom"]').tooltipster({
-        animation: 'fade',
-        delay: 20,
-        speed: 150,
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="bottom"]:not(.tooltipstered)').tooltipster({
         position: 'bottom',
-        maxWidth: 300,
-        theme: 'tooltipster-borderless'
-    });
-    $('[tooltipster="left"]').tooltipster({
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
+        hideOnClick: false,
         animation: 'fade',
         delay: 20,
-        speed: 150,
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
+    });
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="left"]:not(.tooltipstered)').tooltipster({
         position: 'left',
-        maxWidth: 300,
-        theme: 'tooltipster-borderless'
-    });
-    $('[tooltipster="right"]').tooltipster({
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
+        hideOnClick: false,
         animation: 'fade',
         delay: 20,
-        speed: 150,
-        maxWidth: 300,
-        position: 'right',
-        maxWidth: 300,
-        theme: 'tooltipster-borderless'
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
     });
-    $('[tooltipster="right-delay"]').tooltipster({
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="right"]:not(.tooltipstered)').tooltipster({
+        position: 'right',
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
+        hideOnClick: false,
+        animation: 'fade',
+        delay: 20,
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
+    });
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="right-delay"]:not(.tooltipstered)').tooltipster({
+        position: 'right',
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
+        hideOnClick: false,
         animation: 'fade',
         delay: 800,
-        speed: 150,
-        maxWidth: 300,
-        position: 'right',
-        theme: 'tooltipster-borderless'
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
     });
-    $('[tooltipster="top-delay"]').tooltipster({
-        animation: 'fade',
-        delay: 800,
-        speed: 150,
-        maxWidth: 300,
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="top-delay"]:not(.tooltipstered)').tooltipster({
         position: 'top',
-        theme: 'tooltipster-borderless'
-    });
-    $('[tooltipster="left-delay"]').tooltipster({
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
+        hideOnClick: false,
         animation: 'fade',
         delay: 800,
-        speed: 150,
-        maxWidth: 300,
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
+    });
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="left-delay"]:not(.tooltipstered)').tooltipster({
         position: 'left',
-        theme: 'tooltipster-borderless'
-    });
-    $('[tooltipster="bottom-delay"]').tooltipster({
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
+        hideOnClick: false,
         animation: 'fade',
         delay: 800,
-        speed: 150,
-        maxWidth: 300,
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
+    });
+    $(containerToInit).not('.tooltipstered').find('[tooltipster="bottom-delay"]:not(.tooltipstered)').tooltipster({
         position: 'bottom',
-        theme: 'tooltipster-borderless'
+        trigger: 'custom',
+        triggerOpen: tooltipsterTriggerOpen,
+        triggerClose: tooltipsterTriggerClose,
+        hideOnClick: false,
+        animation: 'fade',
+        delay: 800,
+        animationDuration: 150,
+        maxWidth: 280,
+        theme: 'tooltipster-borderless',
+        restoration: 'current'
     });
 }
 
-$(document).ready(function() {
-    tooltipster_init();
+//also init tooltipster immediately, to prevent bugs and calling methods on uninitialised elements
+initTooltipster();
+
+$(document).on('preloadingComplete', function() {
+    initTooltipster();
 });
